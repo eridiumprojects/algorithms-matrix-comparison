8f03758989125a5830ece884bbf3e2e0ee206cd1
kaori <pestrikv1337@gmail.com>
2022-11-05 11:06:25 +0300

    Now the tests are run 100 times and the average values are taken, this helped to correct errors in the indicators of the graphs.

--- src/main/java/com/example/lab1_pestrikv/Main.java
+++ src/main/java/com/example/lab1_pestrikv/Main.java
@@ -1,5 +1,8 @@
 package com.example.lab1_pestrikv;
 
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
 import static com.example.lab1_pestrikv.algorithms.binaryImplementation.binarySearch;
 import static com.example.lab1_pestrikv.algorithms.exponentialImplementation.exponentialSearch;
 import static com.example.lab1_pestrikv.generatedMatrixes.firstMatrixGeneration;
@@ -8,36 +11,56 @@ import static com.example.lab1_pestrikv.algorithms.linearImplementation.linearSe
 
 public class Main {
     public static void main(String[] args) {
-        int rows = (int) Math.pow(2, 1), columns = (int) Math.pow(2, 13); //input rows value
+        int rows = (int) Math.pow(2, 1); //input rows value
         int firstGenerationTarget = 2 * rows + 1;
         int secondGenerationTarget = 16 * rows + 1;
-        int[][] firstMatrix = firstMatrixGeneration(rows, columns);
-        int[][] secondMatrix = secondMatrixGeneration(rows, columns);
-        scoring(firstMatrix, secondMatrix, firstGenerationTarget, secondGenerationTarget);
+        for (int i = 1; i <= 13; i++) {
+            scoring(rows, (int) Math.pow(2, i), firstGenerationTarget, secondGenerationTarget);
+        }
+        for (int i = 1; i <= 13; i++) {
+            scoring(rows, (int) Math.pow(2, i), firstGenerationTarget, secondGenerationTarget);
+        } //we need to take last 13 results in console, it is solves java in-time compilation problem
     }
 
-    public static void scoring(int[][] firstMatrix, int[][] secondMatrix,
+    public static void scoring(int rows, int columns,
                                int firstGenerationTarget, int secondGenerationTarget) {
-        long startBin = System.nanoTime();
-        int binValue = binarySearch(firstMatrix, firstGenerationTarget);
-        long endBin = System.nanoTime();
-        long startExp = System.nanoTime();
-        int expValue = exponentialSearch(firstMatrix, firstGenerationTarget, 2);
-        long endExp = System.nanoTime();
-        long startLinear = System.nanoTime();
-        int linearValue = linearSearch(firstMatrix, firstGenerationTarget);
-        long endLinear = System.nanoTime();
-        long startSecondExp = System.nanoTime();
-        int secondExpVariationValue = exponentialSearch(secondMatrix, secondGenerationTarget, 2);
-        long endSecondExp = System.nanoTime();
-        float resultExp = endExp - startExp;
-        float resultLinear = endLinear - startLinear;
-        float resultBin = endBin - startBin;
-        float resultSecondExp = endSecondExp - startSecondExp;
-        System.out.println("b" + resultBin / 1000000 + "ns "
-                + "exp" + resultExp / 1000000 + "ns "
-                + "standard" + resultLinear / 1000000 + "ns "
-                + "second exp " + resultSecondExp / 1000000 + "ns");
+        long[] binaryResults = new long[100];
+        long[] expResults = new long[100];
+        long[] linearResults = new long[100];
+        long[] secondExpResults = new long[100];
+        for (int i = 0; i < 100; i++) {
+            long start;
+            long end;
+            int[][] firstMatrix = firstMatrixGeneration(rows, columns);
+            int[][] secondMatrix = secondMatrixGeneration(rows, columns);
+            start = System.nanoTime();
+            int binValue = binarySearch(firstMatrix, firstGenerationTarget);
+            end = System.nanoTime();
+            binaryResults[i] = end - start;
+
+            start = System.nanoTime();
+            int expValue = exponentialSearch(firstMatrix, firstGenerationTarget, 2);
+            end = System.nanoTime();
+            expResults[i] = end - start;
+
+            start = System.nanoTime();
+            int linearValue = linearSearch(firstMatrix, firstGenerationTarget);
+            end = System.nanoTime();
+            linearResults[i] = end - start;
+
+            start = System.nanoTime();
+            int secondExpVariationValue = exponentialSearch(secondMatrix, secondGenerationTarget, 2);
+            end = System.nanoTime();
+            secondExpResults[i] = end - start;
+        }
+        OptionalDouble binAvg = Arrays.stream(binaryResults).average();
+        OptionalDouble expAvg = Arrays.stream(expResults).average();
+        OptionalDouble linAvg = Arrays.stream(linearResults).average();
+        OptionalDouble secondExpAvg = Arrays.stream(secondExpResults).average();
+        System.out.println("bin " + binAvg + "ns "
+                + "exp " + expAvg + "ns "
+                + "standard " + linAvg + "ns "
+                + "second exp " + secondExpAvg + "ns");
     }
-}
+    }
 
